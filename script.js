8eb96e3d9b74a0bd9125665cb2966937dd78e253
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-12-21 09:26:05 +0100

    add music

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ function openGift() {
     var nameInput = document.getElementById('nameInput');
     var greeting = document.getElementById('greeting');
     var greetingMessage = document.getElementById('greetingMessage');
+    var christmasMusic = document.getElementById('christmasMusic');
   
     if (nameInput.value.trim() !== '') {
       greetingMessage.textContent = `Merry Christmas, ${nameInput.value}!`;
       greeting.classList.remove('hidden');
+      christmasMusic.play(); // Play the Christmas music
     }
   }
-  
